5f320199966cb90a78ac16775342a6607dc6d77a
eduardojigub <eduardobrunoit@gmail.com>
2022-07-04 13:04:31 -0300

    creating menu system

--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -6,7 +6,34 @@ namespace TextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Open File");
+            Console.WriteLine("2 - Create File");
+            Console.WriteLine("0 - Leave");
+
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
+        }
+        static void Open()
+        {
+
+        }
+        static void Edit()
+        {
+
         }
     }
 }
